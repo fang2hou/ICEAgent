96b87ce1e3122be2b1c0457bc91b73eb744c11eb
Zhou Fang <fang2hou@outlook.com>
2019-11-29 19:54:25 +0900

    Update test collection and rename PlayableTTSService

--- gcloud/text_to_speech.py
+++ gcloud/text_to_speech.py
@@ -1,8 +1,21 @@
+
+#  Copyright (c) 2019. Zhou Fang
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights to
+#  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+#  of the Software, and to permit persons to whom the Software is furnished to
+#  do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
 from google.cloud import texttospeech
 import audioplayer
 
 
 class TTSServiceOptions:
+
     def __init__(self,
                  language_code=None,
                  voice_name=None,
@@ -44,9 +57,9 @@ class TTSService:
             print('Audio content written to file {}'.format(path))
 
 
-class playableTTSService(TTSService):
+class PlayableTTSService(TTSService):
     def __init__(self):
-        super(playableTTSService, self).__init__()
+        super(PlayableTTSService, self).__init__()
         self.p = audioplayer.Mp3Player()
 
     def speak(self, text):
--- modules/icetts.py
+++ modules/icetts.py
@@ -1,19 +1,33 @@
+#  Copyright (c) 2019. Zhou Fang
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights to
+#  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+#  of the Software, and to permit persons to whom the Software is furnished to
+#  do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+
 from flask import Blueprint, request
 
 import gcloud
 
 icetts = Blueprint('icetts', __name__)
 
-tts = gcloud.playableTTSService()
+tts = gcloud.PlayableTTSService()
 
 
 def get_conf_by_key(request, arg):
     try:
-        return request.args[arg]
+        return request.form[arg]
     except KeyError:
         return None
 
 
+# Check the site
+# https://cloud.google.com/text-to-speech/docs/voices
 @icetts.route('/play', methods=['POST'])
 def play_tts():
     if request.method != 'POST':
@@ -26,6 +40,26 @@ def play_tts():
         volume_gain_db=get_conf_by_key(request, 'gain'),
         speaking_rate=get_conf_by_key(request, 'rate'),
     )
+
+    try:
+        text = request.form['text']
+    except KeyError:
+        print('No text found')
+        return 'No text found.'
+
     tts.update_options(tts_opts)
-    tts.speak(request.args['text'])
+    tts.speak(text)
+
+    print('[ICETTS] Now playing\n'
+          'Language Code: {}\n'
+          'Voice Name: {}\n'
+          'Pitch: {}\n'
+          'Volume Gain Db: {}\n'
+          'Speaking Rate: {}\n'
+          'Text: {}\n'
+          '===================================='
+          .format(tts_opts.language_code, tts_opts.voice_name, tts_opts.pitch,
+                  tts_opts.volume_gain_db, tts_opts.speaking_rate, text)
+          )
+
     return 'TTS is playing.'
